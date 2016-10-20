1d3b15cf55921a2085fbe6a50722df701e04f59c
Dave Beck <dacb@uw.edu>
2016-10-20 15:18:24 -0700

    added prints so that the ggplot objects would properly save

--- 2.quality_control_plots.R
+++ 2.quality_control_plots.R
@@ -6,12 +6,12 @@ pdf("2.quality_control_plots.pdf")
 
 # forward
 for(val in 1:length(fnFs)) {
-    plotQualityProfile(paste0(path, fnFs[[1]]))
+    print(plotQualityProfile(paste0(path, fnFs[[1]])))
 }
 
 # reverse
 for(val in 1:length(fnRs)) {
-    plotQualityProfile(paste0(path, fnRs[[1]]))
+    print(plotQualityProfile(paste0(path, fnRs[[1]])))
 }
 
 dev.off()
