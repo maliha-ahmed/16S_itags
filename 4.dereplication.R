91a4e2fe32b7cc367801f067c4331e6afeff29f6
Dave Beck <dacb@uw.edu>
2016-10-28 09:05:04 -0700

    fixed reference to sample.map from prelude.R

--- 4.dereplication.R
+++ 4.dereplication.R
@@ -3,7 +3,7 @@ source("prelude.R")
 derepFs <- derepFastq(filtFs, verbose=TRUE)
 derepRs <- derepFastq(filtRs, verbose=TRUE)
 # Name the derep-class objects by the sample names
-names(derepFs) <- sample.names
-names(derepRs) <- sample.names
+names(derepFs) <- sample.map$samples
+names(derepRs) <- sample.map$samples
 
 save(derepFs, derepRs, file='4.dereplication.Rd')
