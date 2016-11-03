7afd98f691bfb5c1d7ffd60e48250ba3c07789f8
David Beck <dacb@uw.edu>
2016-11-03 09:47:32 -0700

    fixed up sample naming

--- 4.dereplication.R
+++ 4.dereplication.R
@@ -3,7 +3,7 @@ source("prelude.R")
 derepFs <- derepFastq(filtFs, verbose=TRUE)
 derepRs <- derepFastq(filtRs, verbose=TRUE)
 # Name the derep-class objects by the sample names
-names(derepFs) <- sample.map$samples
-names(derepRs) <- sample.map$samples
+names(derepFs) <- sample.map$sample
+names(derepRs) <- sample.map$sample
 
 save(derepFs, derepRs, file='4.dereplication.Rd')
