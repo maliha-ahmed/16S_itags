57b787efa672d8b7536aa43bc36cc92aca0daab4
Dave Beck <dacb@uw.edu>
2016-11-16 10:39:47 -0800

    removed deprecated files and added new items to globals

--- 8.assign_taxonomy.R
+++
@@ -1,9 +0,0 @@
-source("prelude.R")
-
-load("7.chimera_removal.Rd")
-
-taxa <- assignTaxonomy(seqtab.nochim, "rdp_train_set_14.fa.gz")
-colnames(taxa) <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus")
-unname(head(taxa))
-
-save(taxa, file="8.assign_taxonomy.Rd")
