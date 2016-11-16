434f89b989553142f994b86840a35ccd960b2fc3
Dave Beck <dacb@uw.edu>
2016-11-16 10:39:31 -0800

    renamed fasta output path

--- 6.merge_pairs.R
+++ 6.merge_pairs.R
@@ -13,7 +13,7 @@ table(nchar(colnames(seqtab)))
 for (j in seq_len(nrow(seqtab))) {
 	for (i in seq_len(ncol(seqtab))) {
 		if (seqtab[j,i]>0)
-			cat(paste0(">sample=",rownames(seqtab)[j],";seq_",i,";size=",seqtab[j,i],";","\n",colnames(seqtab)[i]),"\n", file=paste0("combined.filtered.fasta"), append=TRUE)
+			cat(paste0(">sample=",rownames(seqtab)[j],";seq_",i,";size=",seqtab[j,i],";","\n",colnames(seqtab)[i]),"\n", file=paste0("data/combined.filtered.fasta"), append=TRUE)
  	}
 }
 
